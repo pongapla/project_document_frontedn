f0a1280d1758de8642d08d089a431d0912b1c32b
pongapla <pongapla@gmail.com>
2023-08-13 22:40:09 +0700

    edit app.uve

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -3,7 +3,7 @@
 <template>
   <div>
     <q-card class="my-card">
-      <img src="https://cdn.quasar.dev/img/mountains.jpg">
+      
     </q-card>
     <h1>Header</h1>
     <q-btn color="primary" label="Primary" />
